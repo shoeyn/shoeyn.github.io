ed8598a2c0904649c2343164da31e5deb0435ca0
Nathan Shoemark <n.shoemark@gmail.com>
2021-02-10 09:23:31 +0000

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true,
   },
   extends: [
-    '@nuxtjs/eslint-config-typescript',
+    // '@nuxtjs/eslint-config-typescript',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
