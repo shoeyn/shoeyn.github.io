ed3733161c880669580bd1c8aedcee57ed8763ad
shoeyn <shoeyn@users.noreply.github.com>
2021-03-26 10:31:34 +0000

    deploy: b7e028909282820a33a71ed13baa21e4974cde17

--- _nuxt/static/1616580950/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616580950",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b9c6e336"}}}}(null,"\u002F"));
