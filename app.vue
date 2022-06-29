ad17dc0571c2e6dae0e69631b7eaaebfbb493388
Nathan Shoemark <n.shoemark@gmail.com>
2022-06-29 11:17:14 +0100

    Dependencies and sitemap

--- app.vue
+++ app.vue
@@ -3,24 +3,24 @@
     <header>
       <nav>
         <NuxtLink
-        to="/"
-        active-class="font-bold"
-        class="mr-4"
-      >
-        Home
-      </NuxtLink>
-      <ContentNavigation v-slot="{ navigation }">
-      <NuxtLink
-        v-for="link of navigation"
-        :key="link._path"
-        :to="link._path"
-        active-class="font-bold"
-        class="mr-4"
-      >
-        {{ link.navTitle || link.title }}
-      </NuxtLink>
-    </ContentNavigation>
-    </nav>
+          to="/"
+          active-class="font-bold"
+          class="mr-4"
+        >
+          Home
+        </NuxtLink>
+        <ContentNavigation v-slot="{ navigation }">
+          <NuxtLink
+            v-for="link of navigation"
+            :key="link._path"
+            :to="link._path"
+            active-class="font-bold"
+            class="mr-4"
+          >
+            {{ link.navTitle || link.title }}
+          </NuxtLink>
+        </ContentNavigation>
+      </nav>
     </header>
     <NuxtPage />
   </div>
