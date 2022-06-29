2e59982f86a0d9ed738b44c8ea90db33c2fb0016
Nathan Shoemark <n.shoemark@gmail.com>
2022-06-29 11:17:14 +0100

    Dependencies and sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,25 +2,29 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    typescript: {
-        shim: false
-    },
-    modules: [
-        '@nuxt/content',
-        '@nuxtjs/tailwindcss',
-        '@nuxtjs/color-mode'
-    ],
-
+  typescript: {
+    shim: false
+  },
+  modules: [
+    '@nuxt/content',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode'
+  ],
+  nitro: {
+    prerender: {
+      routes: ['/sitemap.xml']
+    }
+  },
   colorMode: {
     classSuffix: ''
   },
-    content: {
-        navigation: {
-          fields: ['navTitle']
-        },
-        highlight: {
-          // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
-          theme: 'dracula'
-        }
-      }
+  content: {
+    navigation: {
+      fields: ['navTitle']
+    },
+    highlight: {
+      // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
+      theme: 'dracula'
+    }
+  }
 })
