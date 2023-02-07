5acb4e9e467583a4b200aad861ebb41d7242c9a0
shoeyn <shoeyn@users.noreply.github.com>
2023-02-07 18:02:34 +0000

    deploy: 961366f051e1ad40f67068f793b90196f60b4053

--- _payload.js
+++ _payload.js
@@ -1,1 +1,1 @@
-export default {data:{"content-navigation-8C37fagqQL":[{title:"Hello from Nuxt 3",_path:"\u002Fabout",navTitle:"About"}]},prerenderedAt:1675792942526}
+export default {data:{"content-navigation-8C37fagqQL":[{title:"Hello from Nuxt 3",_path:"\u002Fabout",navTitle:"About"}]},prerenderedAt:1675792953218}
