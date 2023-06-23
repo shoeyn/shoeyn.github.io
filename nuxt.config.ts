a5af69c04faa723323e9b0b987664b3847640f18
Nathan Shoemark <n.shoemark@gmail.com>
2023-06-23 14:59:06 +0100

    Dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,24 @@ export default defineNuxtConfig({
   typescript: {
     shim: false
   },
+
   modules: [
     '@nuxt/content',
     '@nuxtjs/color-mode',
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@nuxt/devtools'
   ],
+
   nitro: {
     prerender: {
       routes: ['/sitemap.xml']
     }
   },
+
   colorMode: {
     classSuffix: ''
   },
+
   content: {
     navigation: {
       fields: ['navTitle']
@@ -23,5 +28,9 @@ export default defineNuxtConfig({
       // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
       theme: 'dracula'
     }
+  },
+
+  devtools: {
+    enabled: true
   }
 })
